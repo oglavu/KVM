2a9a734af6955b6296e3a6d965aa9c9da98ae2e6
oglavu <ognjenmirkovic032@gmail.com>
2025-09-09 00:46:14 +0200

    taskA

--- Guest/guest.c
+++ Guest/guest.c
@@ -1,16 +1,18 @@
+#include <stdint.h>
+
+static void outb(uint16_t port, uint8_t value) {
+	asm("outb %0,%1" : /* empty */ : "a" (value), "Nd" (port) : "memory");
+}
 
 void
 __attribute__((noreturn))
 __attribute__((section(".start")))
 _start(void) {
+	const char *p;
+	uint16_t port = 0xE9;
 
-	/*
-		INSERT CODE BELOW THIS LINE
-	*/
-
-	/*
-		INSERT CODE ABOVE THIS LINE
-	*/
+	for (p = "Hello, world!\n"; *p; ++p)
+		outb(0xE9, *p);
 
 	for (;;)
 		asm("hlt");
