e57061e72fd3a98aa6be6c820437647509d81577
oglavu <ognjenmirkovic032@gmail.com>
2025-09-21 03:50:47 +0200

    test A modified

--- Test/testA1/README.md
+++ Test/testA1/README.md
@@ -1,9 +1,14 @@
 # Test IN/OUT + Mem
 
-App allocates a 4 KB page and asks user to modify it.
+App allocates a 4 KB page and let user access & modify it.
 
-1. User is prompted to enter the number of bytes to be changed. Number entered is rounded to four digits with mandatory leading zeros, e.g. 0008 for 8 digits.
+1. Host is prompted to enter the initial state of Guest's page. Starting from SOF Host provided data is filled in form of bytes. For instance, if Host enters: 23 41 33\n, Guest's memory layout will be as follows: mem[0]=0x23; mem[1]=0x41; mem[2]=0x33
 
-2. User then enters characters starting from the SOF consecutively and once N characters is entered a \n can be entered
+2. Host can permorm the following operations on Guest's memory:
+    u [up] - increment the iterator and print corresponding memory value
+    d [down] - decrement the iterator and print corresponding memory value
+    i XX [insert] - set 0xXX on the iterator's address
+    o XX [offset] - perform relative jump in the array. *Note*: XX is in first complement.
+    r XX [range] - print XX consecutive memory addresses excluding iterator. *Note*: XX is in first complement. 
 
-3. User is promted to enter the number of bytes to be shown starting from the SOF in the same format as in (1)
+3. Host can exit anytime by entering newline instead of command
--- Test/testA1/guest.c
+++ Test/testA1/guest.c
@@ -26,59 +26,101 @@ void puts(const char* s) {
 	for (const char* p = s; *p; putc(*p++));
 }
 
-int atoi(const char *str) {
-	int result = 0;
-	int sign = 1;        // Default sign is positive
+int my_atoi(const char* str) {
 
-	if (*str == '-') {
+    int sign = 1;
+    if (*str == '-') {
         sign = -1;
         str++;
     } else if (*str == '+') {
         str++;
     }
 
-    while (*str >= '0' && *str <= '9') {
+    int result = 0;
+    while (*str) {
         int digit = *str - '0';
         result = result * 10 + digit;
         str++;
     }
 
-    return (int)(result * sign);
+    return result * sign;
 }
 
-char page[4096];
+uint8_t hex_to_byte(const char *s) {
+    uint8_t hi = ('0' <= s[0] && s[0] <= '9') ? s[0]-'0' : 10+s[0]-'a';
+    uint8_t lo = ('0' <= s[1] && s[1] <= '9') ? s[1]-'0' : 10+s[1]-'a';
+    return (uint8_t)((hi << 4) | lo);
+}
+
+void byte_to_hex(uint8_t val, char buf[2]) {
+    const char *digits = "0123456789abcdef";
+    buf[0] = digits[(val >> 4) & 0xF];
+    buf[1] = digits[val & 0xF];
+}
+
+uint8_t page[4096];
 
 void
 __attribute__((noreturn))
 __attribute__((section(".start")))
 _start(void) {
-	char *p;
-	char c;
-	char *s = "4096";
 
-	puts("Unesi koliko menjas (n<4096): ");
+	char s[2];
+	uint8_t *it;
+	
+	puts("Initial fill (XX XX ...)");
 
-	p = s;
-	for (p = s; *p; ++p)
-		*p=getc();
-	getc(); // \n
-	int n = atoi(s);
-
-	puts("Menjaj: ");
-	for (p = page; p < page+n; ++p) {
-		*p = getc();
+	it = page;
+	while(1) {
+		s[0] = getc();
+		if (s[0] == '\n') break;
+		s[1] = getc();
+		if (s[1] == '\n') break;
+		
+		*it++ = hex_to_byte(s);
+		if (getc() == '\n') break; // whitespace
 	}
-	getc(); // \n
 
-	puts("Unesi koliko gledas (n<4096): ");
-	p = s;
-	for (p = s; *p; ++p)
-		*p=getc();
-	getc(); // \n
-	n = atoi(s);
+	puts("Access & Modify memory ( u | d | o XX | i XX )");
 
-	for (p = page; p<page+n; ++p) {
-		putc(*p);
+	char c; it = page;
+	while((c = getc()) != '\n') {
+		if (c == 'u') {
+			byte_to_hex(*++it, s);
+		} else if (c == 'd') {
+			byte_to_hex(*--it, s);
+		} else if (c == 'i') {
+			getc(); // whitespace
+			s[0] = getc();
+			s[1] = getc();
+			*it = hex_to_byte(s);
+		} else if (c == 'o') {
+			getc(); // whitespace
+			s[0] = getc();
+			s[1] = getc();
+			uint8_t off = hex_to_byte(s);
+			it += (off & 0x80) ? -(off & 0x7f) : (off & 0x7f);
+			byte_to_hex(*it, s);
+		} else if (c == 'r') {
+			getc(); // whitespace
+			s[0] = getc();
+			s[1] = getc();
+			uint8_t off = hex_to_byte(s);
+			int sgn = (off & 0x80 ? -1 : 1);
+			uint8_t cnt = off & 0x7f;
+			uint8_t* start = (off & 0x80 ? it-cnt : it+1);
+			for (uint8_t i = 0; i < cnt-1; ++i) {
+				byte_to_hex(*(start + i), s);
+				putc(s[0]);
+				putc(s[1]);
+				putc(' ');
+			}
+			byte_to_hex(*(start+cnt-1), s);
+		}
+		putc(s[0]);
+		putc(s[1]);
+		putc('\n');
+		getc(); // newline
 	}
 
 	for (;;)
