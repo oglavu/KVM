e67f24d77863048d62045c15806d56110a5947d0
oglavu <ognjenmirkovic032@gmail.com>
2025-09-21 17:59:56 +0200

    test c2 implemented

---
+++ Test/testC2/README.md
@@ -0,0 +1,3 @@
+# Filesystem test
+
+Simple app that takes in name of the file and stores its content up to specified size, e.g. 4 KB.
---
+++ Test/testC2/guest.c
@@ -0,0 +1,110 @@
+#include <stdint.h>
+
+#define EOF (int)-1
+
+#define SYS_FOPEN 0
+#define SYS_FCLOSE 1
+#define SYS_FPUTC 2
+#define SYS_FGETC 3
+#define SYS_FTELL 4
+#define SYS_FSEEK 5
+
+static const int FIO_PORT = 0x278;
+static const int CIO_PORT = 0xE9;
+
+static void outb(uint16_t port, uint8_t value) {
+	asm("outb %0,%1" : /* empty */ : "a" (value), "Nd" (port) : "memory");
+}
+
+static void inb(uint16_t port, uint8_t* value) {
+	asm("inb %1,%0" : "=a" (*value) : "Nd" (port) : "memory");
+}
+
+void putc(int c) {
+	outb(CIO_PORT, c);
+}
+
+char getc() {
+	char ret;
+	inb(CIO_PORT, &ret);
+	return ret;
+}
+
+void puts(const char* s) {
+	for (const char* p = s; *p; putc(*p++));
+}
+
+void gets(char* s) {
+	int c;
+    while ((c = getc()) != EOF && c != '\n') {
+        *s++ = (char)c;
+    }
+    *s = '\0';
+}
+
+static inline long syscall(long num, long arg1, long arg2) {
+    register long rax asm("rax") = num;
+    register long rbx asm("rbx") = arg1;
+    register long rcx asm("rcx") = arg2;
+    asm volatile("outb %%al, %3"
+                 : "+a"(rax)
+                 : "b"(rbx), "c"(rcx), "Nd" (FIO_PORT)
+                 : "memory");
+    return rax;
+}
+
+int fopen(const char *filename, const char *mode) {
+	return syscall(SYS_FOPEN, (long)filename, (long)mode);
+}
+
+int fputc(int b, int fd) {
+	return syscall(SYS_FPUTC, (long)b, (long)fd);
+}
+
+int fgetc(int fd) {
+	return syscall(SYS_FGETC, (long)fd, 0);
+}
+
+int fclose(int fd) {
+	return syscall(SYS_FCLOSE, (long)fd, 0);
+}
+
+long ftell(int fd) {
+	return syscall(SYS_FTELL, (long)fd, 0);
+}
+
+int fseek(int fd, long offset) {
+	return syscall(SYS_FSEEK, (long)fd, offset);
+}
+
+void
+__attribute__((noreturn))
+__attribute__((section(".start")))
+_start(void) {
+	const char *p;
+	char c;
+
+	// reading from shared file
+	char filename[100];
+	puts("Enter shared filename: ");
+	gets(filename);
+	puts(filename);
+
+	int fd = fopen(filename, "w");
+	if (fd == 0) {
+		puts("Cannot open file for writing.\n");
+		goto done;
+	}
+	puts("Opened file for writing.\n");
+
+	puts("Enter up to 100 chars to be in file ");
+	gets(filename);
+	
+	for (const char* p=filename; *p; p++) {
+		fputc(*p, fd);
+	}
+
+done:
+	for (;;)
+		asm("hlt");
+}
